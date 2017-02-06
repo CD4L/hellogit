2f800a2f621cf01d6bbc99c59cc11438104f57ad
CD4L <CD4L@users.noreply.github.com>
2017-02-06 12:02:47 -0400

    Updated .h, .cpp but update to driver must be done

--- Cat.cpp
+++ Cat.cpp
@@ -1,39 +1,31 @@
-#include <iostream>
-#include <vector>
+
 #include "Cat.h"
 using namespace std;
 
 
-int linearSearch(auto data, auto key)//prototype
-{
-	for (int i=0; i < data.size(); i++)
-	{
-		if (data[i]==key)
-		return i;
-	}//end for
-	return -1;
-}
 
-int main()
-{
-	Cat cats;
-	vector<Cat>catVec;
-	string catName;
-	int catAge;
 
-	for(int i=0;i<=3;i++)
-	{
-		cout<<"Please enter cat name and age \n";
-		cin>> catName >> catAge;
-		cats.setName(catName);
-		cats.setAge(catAge);
-		catVec.push_back(cats);
-	}
-
-	for(int i=0;i<=3;i++)
-	{
-		cout<<catVec[i].getName()<<"\t";
-		cout<<catVec[i].getAge()<<"\n";
-	}
-	return 0;
-}
+Cat::Cat()
+		{
+			name=" ";
+			age=0.00;
+		}
+		void Cat::setName (string catN)
+			{
+			name=catN;
+			}
+		
+		void Cat::setAge (float catAge)
+		{
+			age=catAge;
+		}
+		
+		string Cat::getName()
+		{
+			return name;
+		}
+		
+		float Cat::getAge()
+		{
+			return age;
+		}
